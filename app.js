5661c55c3ae583167d980bc86acca2d8fe8f2148
Barbarasanchez11 <167989308+Barbarasanchez11@users.noreply.github.com>
2024-09-11 21:09:21 +0200

    finish

--- app.js
+++ app.js
@@ -16,26 +16,22 @@ app.get("/characters", async (req, res) => {
     res.status(500).json({ mensaje: `Personaje no encontrado: ${err.message}` });
   }
 });
-
 app.get("/characters/:name", async (req, res) => {
-  const characterName = req.params.name;
+  const characterName = req.params.name
   try {
     const response = await axios.get(`${urlBase}/?name=${characterName}`);
     const characters = response.data.results;
 
-    if (!characters || characters.length === 0) {
-      return res.status(404).json({ mensaje: "Personaje no encontrado" });
-    }
-
     const charactersList = characters.map(character => {
-      const { name, status, gender, species, image, origin: { name: origin } } = character;
-      return { name, status, gender, species, image, origin };
-    });
-    res.json(charactersList);
-  } catch (err) {
-    res.status(500).json({ mensaje: `Error al buscar el personaje: ${err.message}` });
+      const { name, status, gender, species, image, origin: {name: origin} } = character
+      return { name, status, gender, species, image, origin }
+    })
+    res.json(charactersList)
+  }  catch (err) {
+    res.status(500).json({ mensaje: `Personaje no encontrado: ${err.message}` });
   }
 });
+
 
 app.listen(3000, () => {
   console.log("Express está escuchando en el puerto http://localhost:3000");
